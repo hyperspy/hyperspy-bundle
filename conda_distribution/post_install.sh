702593c6810b60686e2ed99e2ea515e250904075
Eric Prestat <eric.prestat@gmail.com>
2019-10-10 11:14:46 +0100

    Try to fix post install script MacOSX.

--- conda_distribution/post_install.sh
+++ conda_distribution/post_install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+echo "Starting post install script..."
+
 # Activate conda environment
 source ${PREFIX}/bin/activate base
 
+# Desktop integration
 start_jupyter_cm
 
 # Installing the JupyterLab Extension
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+echo "Post install script completed!"
