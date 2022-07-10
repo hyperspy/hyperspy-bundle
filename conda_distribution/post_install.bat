4f1260e5711d9794d3099fb77bec8551e44238c9
Eric Prestat <eric.prestat@gmail.com>
2022-07-10 13:10:27 +0100

    Update docs build

--- conda_distribution/post_install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-:: activate environment
-call %PREFIX%\\Scripts\\activate.bat
-
-:: Add context menu
-start_jupyter_cm
-
--- conda_distribution/pre_uninstall.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: activate environment
-call %PREFIX%\\Scripts\\activate.bat
-
-:: Remove context menu
-start_jupyter_cm --remove
